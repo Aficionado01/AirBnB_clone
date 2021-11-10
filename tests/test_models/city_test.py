e4da4b0a37eaac78737b45328c7af1bcccedb013
CelestineAkpanoko <celestineakpanoko@gmail.com>
2021-11-10 06:08:50 +0000

    City test

--- tests/test_models/city_test.py
+++ tests/test_models/city_test.py
@@ -16,7 +16,7 @@ class CityTest(unittest.TestCase):
         """setup
         """
         cls.dummy_city = City()
-        cls.dummy_city.name = "test"
+        cls.dummy_city.name = 'test'
         cls.dummy_city.state_id = State().id
 
     @classmethod
@@ -29,17 +29,16 @@ class CityTest(unittest.TestCase):
         """test inheritance
         """
         self.assertIsInstance(self.dummy_city, BaseModel)
-        self.assertTrue(hasattr(self.dummy_city, "id"))
-        self.assertTrue(hasattr(self.dummy_city, "created_at"))
-        self.assertTrue(hasattr(self.dummy_city, "updated_at"))
+        self.assertTrue(hasattr(self.dummy_city, 'id'))
+        self.assertTrue(hasattr(self.dummy_city, 'created_at'))
+        self.assertTrue(hasattr(self.dummy_city, 'updated_at'))
 
     def test_attributes(self):
         """test attributes
         """
-        self.assertTrue(hasattr(self.dummy_city, "name"))
-        self.assertTrue(hasattr(self.dummy_city, "state_id"))
+        self.assertTrue(hasattr(self.dummy_city, 'name'))
+        self.assertTrue(hasattr(self.dummy_city, 'state_id'))
 
 
 if __name__ == "__main__":
     unittest.main()
-
