3161c91aa10f4acfa4627e6c9f59e3ae4cd48dec
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-11 17:03:50 +0100

    Added more HBNBCommand tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -101,6 +101,26 @@ class TestHBNBCommand(unittest.TestCase):
                 HBNBCommand().onecmd('quit')
         self.assertEqual(ex.exception.code, 0)
 
+    def test_do_help(self):
+        """Tests the do_help function of the HBNBCommand class.
+        """
+        min_doc_size = 20
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().onecmd('help')
+            self.assertTrue(len(istdout.getvalue()) >= min_doc_size)
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().onecmd('help quit')
+            self.assertTrue(len(istdout.getvalue()) >= min_doc_size)
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().onecmd('help quit 78')
+            self.assertTrue(len(istdout.getvalue()) >= min_doc_size)
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().onecmd('help 78')
+            self.assertEqual(istdout.getvalue(), "*** No help on 78\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().onecmd('help 78 quit')
+            self.assertEqual(istdout.getvalue(), "*** No help on 78 quit\n")
+
     def test_do_create(self):
         """Tests the do_create function of the HBNBCommand class.
         """
@@ -113,7 +133,7 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertEqual(istdout.getvalue(), "** class doesn't exist **\n")
         with patch('sys.stdout', new=StringIO()) as istdout:
             HBNBCommand().onecmd('create Place')
-            self.assertRegex(istdout.getvalue(), r'[-]-\n')
+            self.assertRegex(istdout.getvalue(), r'[0-9a-zA-Z]+(-[0-9a-zA-Z]+)*\n')
 
     def test_do_all(self):
         """Tests the do_all function of the HBNBCommand class.
@@ -127,3 +147,49 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as istdout:
             HBNBCommand().onecmd('all Place')
             self.assertEqual(istdout.getvalue(), "[]\n")
+
+    def test_class_action(self):
+        """Tests the class action syntax of the HBNBCommand class.
+        """
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('foo.a')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: foo.a\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('User.a')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: User.a\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('User.all')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: User.all\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('User.all(')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: User.all(\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('User.all)')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: User.all)\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('User.all(")')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: User.all(\")\n")
+        # with patch('sys.stdout', new=StringIO()) as istdout:
+        #     HBNBCommand().precmd('User.all(1 23)')
+        #     self.assertEqual(istdout.getvalue(), "*** Unknown syntax: User.all(1 23)\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('User.foo()')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: User.foo()\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('.all()')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: .all()\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('User.()')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: User.()\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('User()')
+            self.assertEqual(istdout.getvalue(), "*** Unknown syntax: User()\n")
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('User.show "49faff9a-6318-451f-87b6-910505c55907"')
+            self.assertEqual(
+                istdout.getvalue(),
+                '*** Unknown syntax: User.show "49faff9a-6318-451f-87b6-910505c55907"\n'
+            )
+        with patch('sys.stdout', new=StringIO()) as istdout:
+            HBNBCommand().precmd('foo.all()')
+            self.assertEqual(istdout.getvalue(), "** class doesn't exist **\n")
