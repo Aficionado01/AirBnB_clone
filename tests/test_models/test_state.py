5d90e4d2f4d2893655f292ca2db75b548a47203d
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-11 17:05:18 +0100

    Added more HBNBCommand tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-"""state testcase
+"""A unit test module for the state model.
 """
+from datetime import datetime
 import unittest
-import models
+
+from models.base_model import BaseModel
+from models.state import State
 
 
 class TestState(unittest.TestCase):
     """Represents the test class for the State class.
     """
-    pass
+
+    def test_init(self):
+        """Tests the initialization of the State class.
+        """
+        self.assertIsInstance(State(), BaseModel)
+        self.assertTrue(hasattr(State, 'name'))
+        self.assertIsInstance(State.name, str)
+        self.assertEqual(State(name='California').name, 'California')
+        self.assertEqual(State('Texas', name='Lagos').name, 'Lagos')
