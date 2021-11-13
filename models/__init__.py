e0304a24bef0e219df87581f3ba62082adbffa03
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-13 17:31:00 +0100

    Imported Models in __init__

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,13 @@
 """Modules for working with data sets.
 """
 from models.engine.file_storage import FileStorage
+import models.amenity
+import models.base_model
+import models.city
+import models.place
+import models.review
+import models.state
+import models.user
 
 
 storage = FileStorage()
