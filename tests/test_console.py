f1173a3f9317a813089c4482715fe9806db1d6bd
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-13 22:52:24 +0100

    Updated HBNBCommand's test cases

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,6 +17,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from models.engine.file_storage import FileStorage
 from tests import write_text_file
 
 
@@ -56,71 +57,3 @@ class TestHBNBCommand(unittest.TestCase):
             cons.onecmd('')
             cons.onecmd('  ')
             self.assertEqual(cout.getvalue(), '*** Unknown syntax: ls\n')
-
-    def test_do_EOF(self):
-        """Tests the do_EOF function of the HBNBCommand class.
-        """
-        # running a single EOF
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()):
-                HBNBCommand().onecmd('EOF')
-        self.assertEqual(ex.exception.code, 0)
-        # running EOF after running other commands
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()) as cout:
-                cons = HBNBCommand()
-                cons.onecmd('all 78 User')
-                cons.onecmd('EOF')
-        self.assertEqual(cout.getvalue(), "** class doesn't exist **\n")
-        self.assertEqual(ex.exception.code, 0)
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()) as cout:
-                cons = HBNBCommand()
-                cons.onecmd('show')
-                cons.onecmd('    EOF')
-        self.assertEqual(cout.getvalue(), "** class name missing **\n")
-        self.assertEqual(ex.exception.code, 0)
-        # running commands after running EOF
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()):
-                HBNBCommand().onecmd('    \tEOF\nhelp')
-        self.assertEqual(ex.exception.code, 0)
-        # running EOF with arguments
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()):
-                HBNBCommand().onecmd('EOF 5')
-        self.assertEqual(ex.exception.code, 0)
-
-    def test_do_quit(self):
-        """Tests the do_quit function of the HBNBCommand class.
-        """
-        # running a single quit
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()):
-                HBNBCommand().onecmd('quit')
-        self.assertEqual(ex.exception.code, 0)
-        # running EOF after running other commands
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()) as cout:
-                cons = HBNBCommand()
-                cons.onecmd('all 78 User')
-                cons.onecmd('quit')
-        self.assertEqual(cout.getvalue(), "** class doesn't exist **\n")
-        self.assertEqual(ex.exception.code, 0)
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()) as cout:
-                cons = HBNBCommand()
-                cons.onecmd('show')
-                cons.onecmd('    quit')
-        self.assertEqual(cout.getvalue(), "** class name missing **\n")
-        self.assertEqual(ex.exception.code, 0)
-        # running commands after running quit
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()):
-                HBNBCommand().onecmd('    \tquit\nhelp')
-        self.assertEqual(ex.exception.code, 0)
-        # running quit with arguments
-        with self.assertRaises(SystemExit) as ex:
-            with patch('sys.stdout', new=StringIO()):
-                HBNBCommand().onecmd('quit 5')
-        self.assertEqual(ex.exception.code, 0)
