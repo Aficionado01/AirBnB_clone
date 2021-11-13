6b9d917b6fc04a256957bf2dbb5c6e7b40e06699
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-13 11:50:08 +0100

    Updated reload function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """A module containing the file storage engine.
 """
-import os
-from json import JSONDecoder, JSONEncoder
 from importlib import import_module
+from json import JSONDecoder, JSONEncoder
+import os
 
 
 class FileStorage:
     """Represents the file storage for all data sets.
     """
     __file_path = 'file.json'
-    __objects = {}
+    __objects = dict()
 
     def __init__(self):
         """Initializes a FileStorage instance.
@@ -22,7 +22,7 @@ class FileStorage:
             'City': import_module('models.city').City,
             'Amenity': import_module('models.amenity').Amenity,
             'Place': import_module('models.place').Place,
-            'Review': import_module('models.review').Review,
+            'Review': import_module('models.review').Review
         }
 
     def all(self):
@@ -52,14 +52,15 @@ class FileStorage:
             file.write(JSONEncoder().encode(json_objs))
 
     def reload(self):
-        """Deserializes the JSON file to objects.
+        """Deserializes the JSON file to objects if it exists.
         """
         if os.path.isfile(FileStorage.__file_path):
             file_lines = []
             with open(FileStorage.__file_path, mode='r') as file:
                 file_lines = file.readlines()
-            json_objs = JSONDecoder().decode(''.join(file_lines))
-            base_model_objs = {}
+            file_txt = ''.join(file_lines) if len(file_lines) > 0 else '{}'
+            json_objs = JSONDecoder().decode(file_txt)
+            base_model_objs = dict()
             classes = self.model_classes
             for key, value in json_objs.items():
                 cls_name = value['__class__']
